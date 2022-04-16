a7f309d43b656ce53230e56393cafd16e569e354
diogeneshas <diogenes.henrique.br@gmail.com>
2022-04-16 08:48:55 -0300

    feat: Testes e TDD

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_14_205340) do
+ActiveRecord::Schema.define(version: 2022_04_16_114714) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2022_04_14_205340) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
